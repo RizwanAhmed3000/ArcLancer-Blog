26c58081ca07cf84761e113729e94db7faf85f04
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-07-13 19:18:48 +0500

    navbar done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
+import Link from "next/link"
 import Social from "./SocialLinks/Social"
+import ToggleTheme from "../ToggleTheme/ToggleTheme"
+import AuthLink from "../AuthLink/AuthLink"
 
 const Navbar = () => {
     return (
-        <div className="navContainer py-[10px] ">
-            <div className="social">
+        <div className="navContainer py-[10px] flex items-center justify-between h-[60px]">
+            <div className="social flex flex-1 ">
                 <Social />
             </div>
-            <div className="logo"></div>
-            <div className="links"></div>
+            <div className="logo flex flex-1 justify-center">
+                <h1 className="text-[32px] font-extrabold tracking-widest">Arclancer</h1>
+            </div>
+            <div className="links flex gap-[20px] flex-1 justify-end items-center">
+                <ToggleTheme />
+                <Link href={"/"} className="text-theme-gray font-semibold hover:text-theme-black transition duration-150">Home</Link>
+                <Link href={"/"} className="text-theme-gray font-semibold hover:text-theme-black transition duration-150">Contact</Link>
+                <Link href={"/"} className="text-theme-gray font-semibold hover:text-theme-black transition duration-150">About</Link>
+                {/* <Link href={"/"}>Blog</Link> */}
+                <AuthLink />
+            </div>
         </div>
     )
 }
