65b2ad070b74c425b01d0253e3ca1d37a6ae166c
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-07-13 19:19:11 +0500

    AuthLink & theme toggle

---
+++ src/Components/AuthLink/AuthLink.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+
+const AuthLink = () => {
+    const isLogin = false
+    return (
+        <>
+            {
+                !isLogin ? (
+                    <Link href={"/login"} className='text-theme-gray font-semibold hover:text-theme-black transition duration-150'>
+                        Login
+                    </Link>
+                ) : (
+                    <>
+                        <Link href={"/writeBlog"} className='text-theme-gray font-semibold hover:text-theme-black transition duration-150'>Write</Link>
+                        <span className='text-theme-gray font-semibold hover:text-theme-black transition duration-150 cursor-pointer'>Logout</span>
+                    </>
+                )
+            }
+        </>
+    )
+}
+
+export default AuthLink
