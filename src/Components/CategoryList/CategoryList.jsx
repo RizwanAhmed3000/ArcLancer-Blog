06a787a4cd9d180e35ffe3d48bc7b9567518cd58
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-08-11 15:15:17 +0500

    category list map

--- src/Components/CategoryList/CategoryList.jsx
+++ src/Components/CategoryList/CategoryList.jsx
@@ -1,7 +1,41 @@
+import CategoryBox from "./CategoryBox/CategoryBox"
 
 const CategoryList = () => {
+    const category = [
+        {
+            categoryName: "Sports",
+            icon: "⚽",
+        },
+        {
+            categoryName: "Tech",
+            icon: "💻",
+        },
+        {
+            categoryName: "Politics",
+            icon: "😎",
+        },
+        {
+            categoryName: "World",
+            icon: "🌏",
+        },
+        {
+            categoryName: "Fashion",
+            icon: "💄",
+        },
+    ]
     return (
-        <div>CategoryList</div>
+        <>
+            <h1 className="text-[32px] font-semibold mb-[20px]">
+                Popular Categories
+            </h1>
+            <div className="w-[100%] flex items-center justify-evenly gap-[50px] flex-wrap py-[10px]">
+                {
+                    category.map((item, index)=>(
+                        <CategoryBox icon={item.icon} categoryName={item.categoryName} key={index}/>
+                    ))
+                }
+            </div>
+        </>
     )
 }
 
