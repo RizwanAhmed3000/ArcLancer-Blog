7b75d8d66f593ea9620eb3db8a4c44370717401a
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-08-13 13:16:36 +0500

    CardList

--- src/Components/CardList/CardList.jsx
+++ src/Components/CardList/CardList.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import BlogCard from './BlogCard/BlogCard'
 
 const CardList = () => {
     return (
-        <div className='cardListContainer flex-[5]'>CardList</div>
+        <div className='cardListContainer flex-[5]'>
+            <h1 className='text-[32px] font-semibold mb-[20px]'>Recent Blogs</h1>
+            <div className="postListContainer">
+                <BlogCard />
+                {/* <BlogCard />
+                <BlogCard />
+                <BlogCard /> */}
+            </div>
+        </div>
     )
 }
 
