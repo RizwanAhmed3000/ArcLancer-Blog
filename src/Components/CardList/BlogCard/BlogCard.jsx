d87429c91dcc3a61afede3e275fa9043e79527f5
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-08-14 13:29:28 +0500

    blog card ui

--- src/Components/CardList/BlogCard/BlogCard.jsx
+++ src/Components/CardList/BlogCard/BlogCard.jsx
@@ -1,21 +1,24 @@
 import Image from "next/image"
+import Link from "next/link"
 
 const BlogCard = () => {
     return (
-        <div className="cardConatiner">
-            <div className="img relative w-[100px] h-[100px] object-cover">
-                <Image src={"/Travel.jpg"} fill className="rounded"/>
+        <div className="cardConatiner flex items-center gap-[25px] mb-[50px]">
+            <div className="img relative h-[200px] object-cover flex-1">
+                <Image src={"/Travel.jpg"} fill className="rounded" />
             </div>
-            <div className="text">
-                <div className="details">
-                    <span className="postedAt">8/13/2024</span>
-                    <span className="categoryName">Travel</span>
+            <div className="text flex-[2] flex flex-col gap-[5px]">
+                <div className="details flex gap-[20px] items-center">
+                    <span className="postedAt text-theme-orange">8/13/2024</span>
+                    <span className="categoryName bg-theme-gray-light py-[5px] px-[10px] rounded">Travel</span>
                 </div>
-                <h1>This is the blog main title</h1>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod enim hic ex libero amet quis, temporibus omnis quisquam tenetur dignissimos laborum earum excepturi itaque exercitationem eligendi. Impedit doloribus facere hic repellat laudantium porro aliquam iste eum. Ratione rerum, omnis voluptates fugiat tempore repellat numquam nesciunt aspernatur architecto autem ullam voluptatibus?</p>
-                <div className="readMore">
+                <Link href={"/"}>
+                    <h1 className="text-[22px] font-semibold">This is the blog main title</h1>
+                </Link>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod enim hic ex libero amet quis, temporibus omnis quisquam tenetur dignissimos laborum earum excepturi itaque exercitationem eligendi. Impedit doloribus facere.</p>
+                <Link href={"/"} className="font-semibold underline-offset-4 underline hover:text-theme-orange transition duration-150  w-fit">
                     Read More
-                </div>
+                </Link>
             </div>
         </div>
     )
