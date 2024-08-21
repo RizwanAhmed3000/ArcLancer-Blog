e7a4bc3d28f7eee1e616a4ee0a2cbcac113eaa8d
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-08-21 18:58:04 +0500

    side menu under works

--- src/Components/SideMenu/SideMenu.jsx
+++ src/Components/SideMenu/SideMenu.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const SideMenu = () => {
     return (
-        <div className='sideMenuContainer flex-[2]'>SideMenu</div>
+        <div className='sideMenuContainer flex-[2]'>
+            <h2 className='smallTitle '>{`What's hot`}</h2>
+            <h1 className='mainTitle text-[32px] font-bold'>Popular</h1>
+
+        </div>
     )
 }
 
