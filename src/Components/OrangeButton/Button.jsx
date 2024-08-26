f66a469de1777420dcaee9656188da6df31a4f8a
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-08-26 18:45:58 +0500

    orange btn

---
+++ src/Components/OrangeButton/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({text}) => {
+    return (
+        <button className="bg-theme-orange rounded tabletRg:w-[50%] py-3 px-6 hover:bg-theme-orange-dark transition duration-150">
+            {text}
+        </button>
+    )
+}
+
+export default Button
