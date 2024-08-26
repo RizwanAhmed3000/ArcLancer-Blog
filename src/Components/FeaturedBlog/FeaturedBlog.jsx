2f1d9f7d7124cb8b4e49352f67b0eb57fefe089e
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-08-26 18:46:18 +0500

    orange btn

--- src/Components/FeaturedBlog/FeaturedBlog.jsx
+++ src/Components/FeaturedBlog/FeaturedBlog.jsx
@@ -1,4 +1,6 @@
 import Image from "next/image"
+import Button from "../OrangeButton/Button"
+import Link from "next/link"
 
 const FeaturedBlog = () => {
     return (
@@ -10,7 +12,7 @@ const FeaturedBlog = () => {
             </div>
             <div className="mainTitle tabletSm:bg-theme-white tabletSm:dark:!bg-theme-black tabletSm:w-[100%] tabletSm:flex tabletSm:justify-center tabletRg:mb-[50px] mt-[50px] mb-[100px] tabletSm:mb-[20px] relative">
                 <div className="tabletSm:hidden bg-theme-black dark:bg-theme-white h-[32px] w-1 absolute bottom-[2px] left-[-10px]"></div>
-                <h1 className="text-[20px]">This is the <br className=""/> <b className="text-[36px] leading-8 tracking-widest">Headlines</b></h1>
+                <h1 className="text-[20px]">This is the <br className="" /> <b className="text-[36px] leading-8 tracking-widest">Headlines</b></h1>
             </div>
             <div className="heading">
                 <h1 className="text-[28px] font-semibold tabletSm:text-center">This is the blog heading</h1>
@@ -18,9 +20,9 @@ const FeaturedBlog = () => {
             <div className="smallDesc bg-inherit tabletRg:w-[60%] tabletSm:!w-[100%] w-[40%] text-[15px] mb-[60px] tabletSm:text-center tabletSm:mb-[30px]">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis illo voluptatum eligendi quos, ratione facilis rem fugiat nihil non sapiente cumque labore, velit explicabo tempora quis itaque voluptatem modi delectus.
             </div>
-            <button className="bg-theme-orange rounded tabletRg:w-[50%] py-3 px-6 hover:bg-theme-orange-dark transition duration-150">
-                Read More
-            </button>
+            <Link href={`/`}>
+                <Button text={`Read More`} />
+            </Link>
             <div className="flex">
                 <div className="tabletSm:hidden tabletRg:right-0 tabletRg: absolute right-[20%] top-0 h-[100%] w-[35%] rounded-tl-[10.5rem]">
                     <div className="imgContainer w-[100%] h-[calc(100vh-100px)] relative rounded-tl-[10.5rem]">
