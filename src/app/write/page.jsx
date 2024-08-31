15b781e476bb8d150f568cb70a91855a8bf9b66e
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-08-31 18:57:18 +0500

    write blog page (incomplete)

---
+++ src/app/write/page.jsx
@@ -0,0 +1,42 @@
+"use client"
+import { useState } from "react";
+import { CiCirclePlus, CiImageOn, CiVideoOn } from "react-icons/ci";
+import { FiUpload } from "react-icons/fi";
+import { GoUpload } from "react-icons/go";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.bubble.css";
+
+const WriteBlog = () => {
+
+    const [open, setOpen] = useState(false)
+    const [value, setValue] = useState("")
+
+    return (
+        <div className="container">
+            <input type="text" placeholder="Title" className="w-[100%] bg-transparent text-[58px] outline-none my-[25px] placeholder:text-theme-gray" />
+            <div className="editor flex items-center gap-[20px]">
+                <button className="hover:text-theme-orange transition duration-150" onClick={()=> setOpen(!open)}>
+                    <CiCirclePlus size={40} />
+                </button>
+                {
+                    open && (
+                        <div className="add flex items-center gap-[18px]">
+                            <button className="hover:text-theme-orange transition duration-150">
+                                <CiImageOn size={28}/>
+                            </button>
+                            <button className="hover:text-theme-orange transition duration-150">
+                                <GoUpload size={28}/>
+                            </button>
+                            <button className="hover:text-theme-orange transition duration-150">
+                                <CiVideoOn size={28}/>
+                            </button>
+                        </div>
+                    )
+                }
+                <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell Your Story..."/>
+            </div>
+        </div>
+    )
+}
+
+export default WriteBlog
