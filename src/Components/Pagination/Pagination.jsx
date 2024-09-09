6653aeb463f87d99bc97b0b81d4bfc46a729553c
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-09-09 11:32:04 +0500

    disabled color change

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -8,11 +8,11 @@ const Pagination = ({ page, hasPrev, hasNext }) => {
 
     return (
         <div className=' flex items-center justify-between mb-[25px]'>
-            <button className='w-[100px] bg-theme-orange rounded hover:bg-theme-orange-dark py-[10px] transition duration-150 text-white disabled:bg-theme-orange-dark disabled:cursor-not-allowed' onClick={() => router.push(`?page=${page - 1}`)} disabled={!hasPrev}>
+            <button className='w-[100px] bg-theme-orange rounded hover:bg-theme-orange-dark py-[10px] transition duration-150 text-white disabled:bg-[#e14b158f] disabled:cursor-not-allowed' onClick={() => router.push(`?page=${page - 1}`)} disabled={!hasPrev}>
                 Previous
             </button>
 
-            <button className='w-[100px] bg-theme-orange rounded hover:bg-theme-orange-dark py-[10px] transition duration-150 text-white disabled:bg-theme-orange-dark disabled:cursor-not-allowed' onClick={() => router.push(`?page=${page + 1}`)} disabled={!hasNext}>
+            <button className='w-[100px] bg-theme-orange rounded hover:bg-theme-orange-dark py-[10px] transition duration-150 text-white disabled:bg-[#e14b158f] disabled:cursor-not-allowed' onClick={() => router.push(`?page=${page + 1}`)} disabled={!hasNext}>
                 Next
             </button>
         </div>
