e25a227b5febb1e941d99345d9326c99f275f54b
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-09-09 11:27:51 +0500

    pagination functions

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,11 +1,20 @@
+"use client"
 import React from 'react'
-import Button from '../OrangeButton/Button'
+import { useRouter } from 'next/navigation'
 
-const Pagination = () => {
+const Pagination = ({ page, hasPrev, hasNext }) => {
+
+    const router = useRouter();
+
     return (
         <div className=' flex items-center justify-between mb-[25px]'>
-            <button className='w-[100px] bg-theme-orange rounded hover:bg-theme-orange-dark py-[10px] transition duration-150 text-white'>Previous</button>
-            <button className='w-[100px] bg-theme-orange rounded hover:bg-theme-orange-dark py-[10px] transition duration-150 text-white'>Next</button>
+            <button className='w-[100px] bg-theme-orange rounded hover:bg-theme-orange-dark py-[10px] transition duration-150 text-white disabled:bg-theme-orange-dark disabled:cursor-not-allowed' onClick={() => router.push(`?page=${page - 1}`)} disabled={!hasPrev}>
+                Previous
+            </button>
+
+            <button className='w-[100px] bg-theme-orange rounded hover:bg-theme-orange-dark py-[10px] transition duration-150 text-white disabled:bg-theme-orange-dark disabled:cursor-not-allowed' onClick={() => router.push(`?page=${page + 1}`)} disabled={!hasNext}>
+                Next
+            </button>
         </div>
     )
 }
