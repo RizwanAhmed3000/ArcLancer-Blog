1d0c764ca1161954db1f48fbc411736b61639028
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-09-13 11:11:11 +0500

    some changes

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import React from 'react'
 
 const getData = async (slug) => {
-    // console.log(slug)
+    console.log(slug)
     const res = await fetch(`http://localhost:3000/api/posts/${slug}`, { cache: "no-store" })
     if (!res.ok) {
         throw new Error("Failed to fetch data")
