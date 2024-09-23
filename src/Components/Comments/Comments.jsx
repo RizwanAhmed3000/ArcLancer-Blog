50955d60bc8eaf3853ec45429a55d5c5e360525b
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-09-23 12:19:37 +0500

    post comments

--- src/Components/Comments/Comments.jsx
+++ src/Components/Comments/Comments.jsx
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react"
 import Button from "../OrangeButton/Button"
 import UserDetail from "../UserDetail/UserDetail"
 import useSWR from "swr"
+import { useState } from "react"
 
 const fetcher = async (url) => {
     const res = await fetch(url);
@@ -17,17 +18,26 @@ const fetcher = async (url) => {
 
 const Comments = ({ postId }) => {
     const { status } = useSession()
-    const { data, isLoading } = useSWR(`http://localhost:3000/api/comments?postId=${postId}`, fetcher)
+    const { data, mutate, isLoading } = useSWR(`http://localhost:3000/api/comments?postId=${postId}`, fetcher)
     const isLogin = status === `authenticated` ? true : false
     // console.log(data?.comments)
+    const [desc, setDesc] = useState('')
+
+    const handleSubmit = async () => {
+        await fetch('http://localhost:3000/api/comments', {
+            method: "POST",
+            body: JSON.stringify({ desc, postId })
+        });
+        mutate();
+    }
     return (
         <div className='commContainer my-[25px]'>
             <h1 className='text-[32px] font-semibold'>Comments</h1>
             {
                 isLogin ? (
                     <div className="textArrea flex items-center justify-between gap-[30px] my-[25px]">
-                        <textarea name="comment" placeholder="Write a comment" id="" className="p-[10px] w-[100%] bg-transparent" />
-                        <Button text={`Send`} />
+                        <textarea name="comment" placeholder="Write a comment" id="" className="p-[10px] w-[100%] bg-transparent" onChange={e => { setDesc(e.target.value) }} />
+                        <Button text={`Send`} handler={handleSubmit} />
                     </div>
                 ) : (
                     <div className="textArrea">
@@ -40,7 +50,7 @@ const Comments = ({ postId }) => {
                     isLoading ? "Loading" : data?.comments.map((comment) => (
                         <div className="comment my-[25px]" key={comment.id}>
                             <UserDetail type={`small`} userData={comment.user} commentDate={comment.createdAt} />
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum ipsum obcaecati sint aut, incidunt tempora eos accusantium ipsa ad asperiores ea recusandae debitis nobis, doloremque, aspernatur harum modi. Cumque, ut.</p>
+                            <p>{comment.desc}</p>
                         </div>
                     ))
                 }
--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,6 +1,8 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
+// GET COMMENTS
 export const GET = async (req) => {
 
     const { searchParams } = new URL(req.url)
@@ -23,6 +25,56 @@ export const GET = async (req) => {
             {
                 status: "Success",
                 comments,
+            },
+            {
+                status: 200
+            }
+        ))
+    } catch (error) {
+        // console.log(error)
+        return new NextResponse(JSON.stringify(
+            {
+                status: "Failed",
+                message: "Something went wrong",
+                data: error
+            },
+            {
+                status: 500
+            }
+        ))
+    }
+}
+
+//POST COMMENT
+export const POST = async (req) => {
+
+    const session = await getAuthSession();
+    // console.log(session);
+    // console.log(session.user);
+    if (!session) {
+        return new NextResponse(JSON.stringify(
+            {
+                status: "Failed",
+                message: "You are not authenticated for this operation",
+            },
+            {
+                status: 401
+            }
+        ))
+    }
+    try {
+        const body = await req.json();
+        // console.log(body)
+        const comment = await prisma.comment.create({
+            data: { ...body, userEmail: session.user.email }
+        })
+
+        // console.log(comments)
+
+        return new NextResponse(JSON.stringify(
+            {
+                status: "Success",
+                comment,
             },
             {
                 status: 200
